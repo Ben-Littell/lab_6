5f8911800ae9d8d27615036c9e1443bd6161d273
benlittell <blittell13@gmail.com>
2021-11-01 22:06:12 -0400

    partially done spawning enemies

--- stuff.py
+++ stuff.py
@@ -20,6 +20,7 @@ WIDTH = 700
 HEIGHT = 500
 SIZE = (WIDTH, HEIGHT)
 FPS = 60
+enemy_list = []
 
 
 ##############################################################################
@@ -54,6 +55,43 @@ class Background:
             change_x += 140
 
 
+class Player:
+    def __init__(self, display, x, y, width, height, x_speed=0, y_speed=0):
+        self.display = display
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.x_speed = x_speed
+        self.y_speed = y_speed
+
+    def draw_player(self):
+        pygame.draw.rect(self.display, RED, (self.x, self.y, self.width, self.height))
+
+    def update(self):
+        self.x += self.x_speed
+        self.y += self.y_speed
+        if self.x <= 0 or self.x >= WIDTH - self.width:
+            self.x += -1 * self.x_speed
+        if self.y <= 0 or self.y >= HEIGHT - self.height:
+            self.y += -1 * self.y_speed
+
+
+class Enemies:
+    def __init__(self, e_height, e_width, s_width, velocity=5):
+        self.e_height = e_height
+        self.e_width = e_width
+        self.s_width = s_width
+        self.velocity = velocity
+
+    def draw_enemies(self):
+        s_place = random.randrange(self.s_width - self.e_width)
+        pygame.draw.rect(screen, BLACK, (s_place, 10, self.e_height, self.e_width))
+
+    def update(self):
+        self.e_height += self.velocity
+
+
 ##############################################################################
 
 pygame.init()
@@ -64,7 +102,13 @@ pygame.display.set_caption('Collision Detection Intro')
 clock = pygame.time.Clock()
 ###################################
 background = Background(BLACK, CERULEAN, WHITE, 30)
+player1 = Player(screen, 200, HEIGHT - 80, 45, 45)
+for numb in range(50):
+    enemy = Enemies(10, -10, WIDTH - 20)
+    enemy_list.append(enemy)
+enemy1 = Enemies(10, 10, WIDTH)
 ###################################
+
 running = True
 # game loop
 while running:
@@ -72,9 +116,45 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT:
+                player1.x_speed = 0
+                # player.y_speed = 0
+            if event.key == pygame.K_LEFT:
+                player1.x_speed = 0
+                # player.y_speed = 0
+            # if event.key == pygame.K_UP:
+            #     player1.y_speed = 0
+            #     # player.x_speed = 0
+            # if event.key == pygame.K_DOWN:
+            #     player1.y_speed = 0
+            #     # player.x_speed = 0
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                player1.x_speed = 5
+                # player.y_speed = 0
+            if event.key == pygame.K_LEFT:
+                player1.x_speed = -5
+                # player.y_speed = 0
+            # if event.key == pygame.K_UP:
+            #     player1.y_speed = -5
+            #     # player.x_speed = 0
+            # if event.key == pygame.K_DOWN:
+            #     player1.y_speed = 5
+            #     # player.x_speed = 0
+            if event.key == pygame.K_SPACE:
+                player1.x_speed = 0
+                player1.y_speed = 0
 
     screen.fill(WHITE)
     background.draw_back()
+    player1.draw_player()
+    player1.update()
+    for item in enemy_list:
+        item.draw_enemies()
+        item.update()
+    enemy1.draw_enemies()
     pygame.display.flip()
 
     clock.tick(FPS)
